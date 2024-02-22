356ba552c2137e79bd447007dcdf061fe5dfb191
Ênnya Gomes <ennyagomesoc@gmail.com>
2024-02-22 13:39:38 -0300

    .

--- Diretivas/diretivas-proj/card.component.ts
+++ Diretivas/diretivas-proj/card.component.ts
@@ -5,11 +5,30 @@ import { Component } from "@angular/core";
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'] 
 })
-export class AppComponent{
-    title = 'life-cycle';
-    isAliveCheckSample:boolean = true
+export class CardComponent implements OnInit{
+    produtos:string[] = []
+    menuType:string = "user"
 
-    disposeCheckSample():void{
-        this.isAliveCheckSample = false
+    constructor(){
+        this.produtos = [
+            "mouse",
+            "teclado",
+            "cabo",
+            "fonte"
+        ]
     }
+
+    ngOnInit(): void {
+
+    }
+
+    adicionar(){
+        this.produtos.pop()
+    }
+
+    remover(index: number){
+        this.produtos.splice(index, 1)
+        // splice retira o item
+    }
+  
 }
