a8f52337c88f0b3915164cdea7b5898dea791700
Ênnya Gomes <ennyagomesoc@gmail.com>
2024-02-23 00:44:39 -0300

    .

--- DiretivasOficial/diretivas-proj/src/app/card/card.component.ts
+++ DiretivasOficial/diretivas-proj/src/app/card/card.component.ts
@@ -1,9 +1,9 @@
-import { Component } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-    selector: 'app-root ',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'] 
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.css'] 
 })
 export class CardComponent implements OnInit{
     produtos:string[] = []
